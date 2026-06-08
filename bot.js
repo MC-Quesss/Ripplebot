075f78d3b05fa59bd281daa914324a76d8f58522
Matt Q <matthew.quesada@kipuhealth.com>
2026-06-07 21:26:12 -0400

    spiders are hostile

--- bot.js
+++ bot.js
@@ -675,7 +675,7 @@ const POTATO_SWEEP_POINTS = [
 const FIELD_BOUNDS = { xMin: -287, xMax: -279, zMin: 559, zMax: 565 }
 const NORTH_FIELD_BOUNDS = { xMin: -287, xMax: -279, zMin: 551, zMax: 557 }
 const HOSTILE_NAMES = new Set([
-  'zombie', 'skeleton', 'creeper', 'witch', 'enderman',
+  'zombie', 'skeleton', 'creeper', 'witch', 'enderman', 'spider',
   'slime', 'husk', 'drowned', 'phantom', 'stray', 'cave_spider',
 ])
 
